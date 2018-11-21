e0cf4200aeb57bb720382b407b7063ce0836f57a
Samler <1045704373@qq.com>
2018-11-21 10:39:18 +0800

    Update BackgroundPicForBing.js

--- BackgroundPicForBing.js
+++ BackgroundPicForBing.js
@@ -1,11 +1,10 @@
-/** BackgroundPicForBing-v1.0 MIT License By https://www.samler.cn */
 ;!function(e){
 	var url = 'https://cn.bing.com/ImageResolution.aspx?w='+e.innerWidth+'&h='+e.innerHeight,
-		bbfp = new Image();
-		bbfp.src = url;
-		bbfp.style = 'display: none';
+		bpfb = new Image();
+		bpfb.src = url;
+		bpfb.style = 'display: none';
 	
-	bbfp.onload = function(){
+	bpfb.onload = function(){
 		bp = document.createElement('div');
 		bp.setAttribute('id', 'bingBackgroundPic');
 		bp.setAttribute('style', 'width: 100vw; height: 100vh; position: fixed; top: 0px; left: 0px; background-image: url('+url+'); background-size: cover; display: none; opacity: 0; z-index: -9999; transition: opacity .3s');
